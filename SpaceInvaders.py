09d3fa79d0429a16d5d039ba847ce0dc1de13062
kaarel <kaarel.toomet@gmail.com>
2018-03-10 18:09:34 -0800

    added ufos

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -3,13 +3,16 @@ import random as r
 pg.init()
 pic = pg.image.load("kausyarcher.png")
 arw = pg.image.load("arrow.png")
-invader1 = pg.image.load("invader1.png")
+ufo1 = pg.image.load("invader1.png")
 ray = pg.image.load("ray.png")
 pg.font
 screen = pg.display.set_mode((0,0), pg.RESIZABLE)
 screenw = screen.get_width()
 screenh = screen.get_height()
 pg.display.set_caption("Space Invaders")
+points = 0
+u1tick = 0
+u1max = 300
 do = True
 spd = 6
 left = True
@@ -26,6 +29,7 @@ gameover = False
 gf = False
 player = pg.sprite.Group()
 arrows = pg.sprite.Group()
+ufos1 = pg.sprite.Group()
 class Player(pg.sprite.Sprite):
     def __init__(self,x,y):
         pg.sprite.Sprite.__init__(self)
@@ -62,10 +66,42 @@ class Proj(pg.sprite.Sprite):
         if self.y + self.vel <= screenh-96 and self.rect.y + self.vel >= 0:
             self.y += self.vel
             self.rect.y = int(self.y)
+class UFO(pg.sprite.Sprite):
+    def __init__(self, x, y, vel, img, shootdelay, bpic, bspd, hp, val):
+        pg.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.x = float(x)
+        self.y = float(y)
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+        self.vel = vel
+        self.bullet = bpic
+        self.bulletvel = bspd
+        self.maxtick = shootdelay
+        self.tick = 0
+        self.health = hp
+        self.value = val
+        self.wek = False
+        self.tick = 0
+    def update(self, col):
+        global points
+        self.tick += 1
+        if self.tick >= self.maxtick:
+            self.tick = 0
+            #arrows.add(Proj(kausy.getx()+28, screenh-96,-10,arw))
+        if self.x + self.vel <= screenw-96 and self.x + self.vel >= 0:
+            self.x += self.vel
+            self.rect.x = int(self.x)
+        else:
+            self.vel = -self.vel
 def reset():
+    global lifes, player, arrows, ufos1
     lifes = 5
     player.empty()
-    kausy = Player(screenw/2,screenh/2)
+    arrows.empty()
+    ufos1.empty
+    kausy = Player(screenw/2,screenh-96)
     player.add(kausy)
 kausy = Player(screenw/2,screenh-96)
 player.add(kausy)
@@ -129,8 +165,12 @@ while do:
                 if event.key == pg.K_r:
                     gameover = False
                     reset()
+    col1 = pg.sprite.groupcollide(arrows, ufos1, True, True)
+    for s in col1.keys():
+        if len(col1[s]) > 0:
+            points += 1
     screen.fill((127,127,127))
-    score = ("Lifes: " + str(lifes))
+    score = ("Lifes: " + str(lifes) + " Score: " + str(points))
     text = font.render(score, True, (255,255,255))
     text_rect = text.get_rect()
     text_rect.centerx = screen.get_rect().centerx
@@ -140,7 +180,14 @@ while do:
     player.draw(screen)
     arrows.update()
     arrows.draw(screen)
+    ufos1.update(col1)
+    ufos1.draw(screen)
     pg.display.update()
+    u1tick += 1
+    if u1tick >= u1max: #self, x, y, vel, img, shootdelay, bpic, bspd, hp, val
+        u1tick = 0
+        ufos1.add(UFO(r.randint(0,screenw-96),r.randint(0, 256), 4, ufo1,
+                      60, ray, 16, 1, 1))
     if not gf:
         timer.tick(60)
 
